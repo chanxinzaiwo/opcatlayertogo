859aa6911ff1fcbc1726c59ffe9ec4d5969028f9
OPCAT_Dev <tt21945@gmail.com>
2025-12-10 05:53:50 +0800

    重构文件结构：将组件移动到src目录

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+
+import React from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Problem from './components/Problem';
+import Concept from './components/Concept';
+import Architecture from './components/Architecture';
+import BitcoinScript from './components/BitcoinScript';
+import Developer from './components/Developer';
+import UseCases from './components/UseCases';
+import Vision from './components/Vision';
+import WhyItWins from './components/WhyItWins';
+import Consensus from './components/Consensus';
+import Comparison from './components/Comparison';
+import Action from './components/Action';
+import Footer from './components/Footer';
+import HalvingCountdown from './components/HalvingCountdown';
+import { LanguageProvider } from './contexts/LanguageContext';
+
+const AppContent: React.FC = () => {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="font-sans antialiased text-stone-900 selection:bg-orange-200 selection:text-orange-900">
+      <Navigation scrollToSection={scrollToSection} />
+      <main>
+        <Hero scrollToSection={scrollToSection} />
+        <Problem />
+        <BitcoinScript />
+        <Concept />
+        <Architecture />
+        <Developer />
+        <UseCases />
+        <WhyItWins />
+        <Vision />
+        <Comparison />
+        <Consensus />
+        <HalvingCountdown />
+        <Action />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <LanguageProvider>
+      <AppContent />
+    </LanguageProvider>
+  );
+};
+
+export default App;
